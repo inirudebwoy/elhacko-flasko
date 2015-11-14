f7a3bddd5e7d728c8bb60152452bb7304d666724
Michael Klich <michal@michalklich.com>
2015-11-14 17:38:06 +0000

    Save uuid in db

--- fapp.py
+++ fapp.py
@@ -1,15 +1,38 @@
+import shelve
 from uuid import uuid4
 
 from flask import Flask
 
 
 app = Flask(__name__)
+DB_PATH = 'db.shelve'
 
 
-@app.route('/')
-def generate_uuid():
-    uuid = uuid4()
-    return uuid.hex
+def open_db():
+    return shelve.open(DB_PATH)
+
+
+def generate_uuid_hex():
+    uuid = uuid4().hex
+    db = open_db()
+
+    if uuid in db:
+        return generate_uuid_hex()
+
+    # create bucket for UUID
+    db[uuid] = {}
+
+    db.close()
+    return uuid
+
+
+@app.route('/uuid')
+def get_uuid():
+    return generate_uuid_hex()
+
+
+def store_image(uuid):
+    pass
 
 
 if __name__ == '__main__':
